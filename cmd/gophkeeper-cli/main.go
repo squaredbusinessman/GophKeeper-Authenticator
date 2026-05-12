98aa5f99ac6f7d740ec5e2a421d8f5da6f7ec9e5
EVGENY ANTROPOV <burdaonelove@gmail.com>
2026-05-12 11:32:39 +0300

    feat: реализует команду получения версии приложения

--- cmd/gophkeeper-cli/main.go
+++ cmd/gophkeeper-cli/main.go
@@ -1,3 +1,38 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/squaredbusinessman/gophkeeper-authenticator/internal/shared/version"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	switch os.Args[1] {
+	case "version":
+		printVersion()
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
+		printUsage()
+		os.Exit(1)
+	}
+}
+
+func printVersion() {
+	info := version.Get()
+
+	fmt.Printf("GophKeeper CLI\n")
+	fmt.Printf("Version: %s\n", info.Version)
+	fmt.Printf("Build date: %s\n", info.BuildDate)
+	fmt.Printf("Commit: %s\n", info.Commit)
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  gophkeeper version")
+}
